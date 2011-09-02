541bc06923d9cc49c18bc0bd6ca9f9d883c49cee
andjelko-so <milosdjikic@gmail.com>
2011-09-02 09:38:25 +0200

    corrections

--- WebContent/modules/locale/locale.datepicker.js
+++ WebContent/modules/locale/locale.datepicker.js
@@ -36,30 +36,30 @@ $.datepicker.regional['drupal-locale'] = {
   ],
   dayNames: [
     Drupal.t('Sunday'),
-    Drupal.t('Monday')
-    Drupal.t('Tuesday')
-    Drupal.t('Wednesday')
-    Drupal.t('Thursday')
-    Drupal.t('Friday')
+    Drupal.t('Monday'),
+    Drupal.t('Tuesday'),
+    Drupal.t('Wednesday'),
+    Drupal.t('Thursday'),
+    Drupal.t('Friday'),
     Drupal.t('Saturday')
   ],
   dayNamesShort: [
-    Drupal.t('Sun')
-    Drupal.t('Mon')
-    Drupal.t('Tue')
-    Drupal.t('Wed')
-    Drupal.t('Thu')
-    Drupal.t('Fri')
+    Drupal.t('Sun'),
+    Drupal.t('Mon'),
+    Drupal.t('Tue'),
+    Drupal.t('Wed'),
+    Drupal.t('Thu'),
+    Drupal.t('Fri'),
     Drupal.t('Sat')
   ],
   dayNamesMin: [
-    Drupal.t('Su')
-    Drupal.t('Mo')
-    Drupal.t('Tu')
-    Drupal.t('We')
-    Drupal.t('Th')
-    Drupal.t('Fr')
-    Drupal.t('Sa')
+    Drupal.t('Su'),
+    Drupal.t('Mo'),
+    Drupal.t('Tu'),
+    Drupal.t('We'),
+    Drupal.t('Th'),
+    Drupal.t('Fr'),
+    Drupal.t('Sa'),
   ],
   dateFormat: Drupal.t('mm/dd/yy'),
   firstDay: Drupal.settings.jqueryuidatepicker.firstDay,
